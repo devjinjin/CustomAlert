6e982489c2b6dd05af3fe49f7051b5fe4c7dbdaa
devjinjin <devjinjin@gmail.com>
2021-05-24 15:49:05 +0900

    Initial Commit

---
+++ CustomAlert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomAlert
+//
+//  Created by jylee-mac on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
